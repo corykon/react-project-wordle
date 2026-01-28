47d3cb88500f54fe9235b0466f33df86c2ecf835
Cory Fugate <cory.fugate@healthcatalyst.com>
2026-01-28 11:04:38 -0700

    exercise 1: add the guess input form

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import GuessInput from "../GuessInput/GuessInput";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info('answer:', answer);
 
 function Game() {
-  return <>Put a game here!</>;
+    return <GuessInput />;
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function GuessInput() {
+    const [guess, setGuess] = React.useState('');
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('Submitted guess:', guess);
+        setGuess('');
+    }
+    return <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input id="guess-input" type="text" value={guess} onChange={e => setGuess(e.target.value.toLocaleUpperCase().trim().slice(0, 5))} pattern="[A-Za-z]{5}" title="Five letter word" required />
+    </form>;
+}
+
+export default GuessInput;
